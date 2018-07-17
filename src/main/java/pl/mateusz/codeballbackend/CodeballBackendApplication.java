dd6b532c486ef7385475edd20f2533f0aa3cab8b
Mateusz Siwiec <mateuszsiwiec3@gmail.com>
2018-07-17 13:24:35 +0200

    initialize project

---
+++ src/main/java/pl/mateusz/codeballbackend/CodeballBackendApplication.java
@@ -0,0 +1,12 @@
+package pl.mateusz.codeballbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeballBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeballBackendApplication.class, args);
+	}
+}
